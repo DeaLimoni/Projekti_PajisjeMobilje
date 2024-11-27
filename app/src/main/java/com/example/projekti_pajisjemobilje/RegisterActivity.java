c525631ecdf16e07699caff467194a394385fe47
Dea Limoni <dea.limoni@student.uni-pr.edu>
2024-11-27 18:46:33 +0100

    update

--- app/src/main/java/com/example/projekti_pajisjemobilje/RegisterActivity.java
+++ app/src/main/java/com/example/projekti_pajisjemobilje/RegisterActivity.java
@@ -1,6 +1,13 @@
 package com.example.projekti_pajisjemobilje;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +17,38 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private EditText editTextRegisterFullName, editTextRegisterEmail, editTextRegisterDoB,
+            editTextRegisterMobile, editTextRegisterPwd, editTextRegisterConfirmPwd;
+    private ProgressBar progressBar;
+    private RadioGroup radioGroupRegisterGender;
+    private RadioButton radioButtonRegisterGenderSelected;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        getSupportActionBar().setTitle("Register");
+
+        Toast.makeText(RegisterActivity.this, "You can register now",Toast.LENGTH_LONG).show();
+        editTextRegisterFullName = findViewById(R.id.editText_register_full_name);
+        editTextRegisterEmail = findViewById(R.id.editText_register_email);
+        editTextRegisterDoB = findViewById(R.id.editText_register_dob);
+        editTextRegisterMobile = findViewById(R.id.editText_register_mobile);
+        editTextRegisterPwd = findViewById(R.id.editText_register_password);
+        editTextRegisterConfirmPwd = findViewById(R.id.editText_register_confirm_password);
+        //radio button for gender
+        radioGroupRegisterGender = findViewById(R.id.radio_group_register_gender);
+        radioGroupRegisterGender.clearCheck();
+
+
+        Button buttonRegister = findViewById(R.id.button_register);
+        buttonRegister.setOnClickListener(new View.OnClickListener(){
+        @Override
+            public void onClick(View v){
+
+        }
+
+        }
     }
 }
