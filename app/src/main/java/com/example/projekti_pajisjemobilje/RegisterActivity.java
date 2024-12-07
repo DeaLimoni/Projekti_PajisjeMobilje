726ad102af18b22a788daec3ac7c0774acd79add
Dea Limoni <dea.limoni@student.uni-pr.edu>
2024-12-07 13:19:15 +0100

    update

--- app/src/main/java/com/example/projekti_pajisjemobilje/RegisterActivity.java
+++ app/src/main/java/com/example/projekti_pajisjemobilje/RegisterActivity.java
@@ -32,7 +32,7 @@ public class RegisterActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle("Register");
 
-        Toast.makeText(RegisterActivity.this, "You can register now",Toast.LENGTH_LONG).show();
+        Toast.makeText(RegisterActivity.this, "You can register now", Toast.LENGTH_LONG).show();
         editTextRegisterFullName = findViewById(R.id.editText_register_full_name);
         editTextRegisterEmail = findViewById(R.id.editText_register_email);
         editTextRegisterDoB = findViewById(R.id.editText_register_dob);
@@ -45,46 +45,85 @@ public class RegisterActivity extends AppCompatActivity {
 
 
         Button buttonRegister = findViewById(R.id.button_register);
-        buttonRegister.setOnClickListener(new View.OnClickListener(){
-        @Override
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
 
-            int selectedGenderId = radioGroupRegisterGender.getCheckedRadioButtonId();
-            radioButtonRegisterGenderSelected = findViewById(selectedGenderId);
+                int selectedGenderId = radioGroupRegisterGender.getCheckedRadioButtonId();
+                radioButtonRegisterGenderSelected = findViewById(selectedGenderId);
 
 
 //obtain the entered data
-            String textFullName = editTextRegisterFullName.getText().toString();
-            String textEmail = editTextRegisterEmail.getText().toString();
-            String textDob = editTextRegisterDoB.getText().toString();
-            String textMobile = editTextRegisterMobile.getText().toString();
-            String textPwd = editTextRegisterPwd.getText().toString();
-            String textConfirmed = editTextRegisterConfirmPwd.getText().toString();
-            String textGender;
-            if (TextUtils.isEmpty(textFullName)) {
-                Toast.makeText(RegisterActivity.this, "Please enter your full name", Toast.LENGTH_LONG).show();
-                editTextRegisterFullName.setError("Full Name is required");
-                editTextRegisterFullName.requestFocus();
-            } else if (TextUtils.isEmpty(textEmail)) {
-                Toast.makeText(RegisterActivity.this, "Please enter your email", Toast.LENGTH_LONG).show();
-            editTextRegisterEmail.setError("Email is required");
-                editTextRegisterEmail.requestFocus();
-            }else if(!Patterns.EMAIL_ADDRESS.matcher(textEmail).matches()) {
-                Toast.makeText(RegisterActivity.this, "Please re-enter your email", Toast.LENGTH_LONG).show();
-                editTextRegisterEmail.setError("Valid email is required");
-                editTextRegisterEmail.requestFocus();
-            }else if (TextUtils.isEmpty(textDob)) { // Përdorni textDob këtu
-                Toast.makeText(RegisterActivity.this, "Please enter your date of birth", Toast.LENGTH_LONG).show();
-                editTextRegisterDoB.setError("Date of Birth is required");
-                editTextRegisterDoB.requestFocus();
-            }else if(radioButtonRegisterGenderSelected.getCheckedRadioButtonId() == -1){
-                Toast.makeText(RegisterActivity.this, "Please select your gender", Toast.LENGTH_LONG).show();
-                radioButtonRegisterGenderSelected.setError("gender is required");
-                radioButtonRegisterGenderSelected.requestFocus();
-        }else if(TextUtils.isEmpty(textMobile)){
+                String textFullName = editTextRegisterFullName.getText().toString();
+                String textEmail = editTextRegisterEmail.getText().toString();
+                String textDob = editTextRegisterDoB.getText().toString();
+                String textMobile = editTextRegisterMobile.getText().toString();
+                String textPwd = editTextRegisterPwd.getText().toString();
+                String textConfirmPwd = editTextRegisterConfirmPwd.getText().toString().trim();
+                String textGender;
 
+                if (TextUtils.isEmpty(textFullName)) {
+                    Toast.makeText(RegisterActivity.this, "Please enter your full name", Toast.LENGTH_LONG).show();
+                    editTextRegisterFullName.setError("Full Name is required");
+                    editTextRegisterFullName.requestFocus();
+                } else if (TextUtils.isEmpty(textEmail)) {
+                    Toast.makeText(RegisterActivity.this, "Please enter your email", Toast.LENGTH_LONG).show();
+                    editTextRegisterEmail.setError("Email is required");
+                    editTextRegisterEmail.requestFocus();
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(textEmail).matches()) {
+                    Toast.makeText(RegisterActivity.this, "Please re-enter your email", Toast.LENGTH_LONG).show();
+                    editTextRegisterEmail.setError("Valid email is required");
+                    editTextRegisterEmail.requestFocus();
+                } else if (TextUtils.isEmpty(textDob)) { // Përdorni textDob këtu
+                    Toast.makeText(RegisterActivity.this, "Please enter your date of birth", Toast.LENGTH_LONG).show();
+                    editTextRegisterDoB.setError("Date of Birth is required");
+                    editTextRegisterDoB.requestFocus();
+                } else if (radioGroupRegisterGender.getCheckedRadioButtonId() == -1) {
+                    Toast.makeText(RegisterActivity.this, "Please select your gender", Toast.LENGTH_LONG).show();
+                    // Nuk mund të përdorni `radioButtonRegisterGenderSelected` sepse ende nuk është inicializuar
+                } else {
+                    // Inicializo `radioButtonRegisterGenderSelected` vetëm kur ka një gjini të përzgjedhur
+                    radioButtonRegisterGenderSelected = findViewById(radioGroupRegisterGender.getCheckedRadioButtonId());
+                    // Merr tekstin e gjinisë së përzgjedhur
+                    textGender = radioButtonRegisterGenderSelected.getText().toString();
+                } else if (TextUtils.isEmpty(textMobile)) {
+                    Toast.makeText(RegisterActivity.this, "Please enter your mobile no.", Toast.LENGTH_LONG).show();
+                    editTextRegisterMobile.setError("Mobile no. is required");
+                } else if (textMobile.length() != 10) {
+                    Toast.makeText(RegisterActivity.this, "Please re-enter your mobile no.", Toast.LENGTH_LONG).show();
+                    editTextRegisterMobile.setError("mobile no. should be 10 digits");
+                    editTextRegisterMobile.requestFocus();
+                } else if (TextUtils.isEmpty(textPwd)) {
+                    Toast.makeText(RegisterActivity.this, "Please enter your password", Toast.LENGTH_LONG).show();
+                    editTextRegisterPwd.setError("Password is required");
+                    editTextRegisterPwd.requestFocus();
+                } else if (textPwd.length() < 6) {
+                    Toast.makeText(RegisterActivity.this, "Password should be at least 6 digits", Toast.LENGTH_LONG).show();
+                    editTextRegisterPwd.setError("Password too weak");
+                    editTextRegisterPwd.requestFocus();
+                } else if (TextUtils.isEmpty(textConfirmPwd)) {
+                    Toast.makeText(RegisterActivity.this, "Please confirm your password", Toast.LENGTH_LONG).show();
+                    editTextRegisterConfirmPwd.setError("Password confirm is required");
+                    editTextRegisterConfirmPwd.requestFocus();
+                } else if (!textPwd.equals(textConfirmPwd)) {
+                    Toast.makeText(RegisterActivity.this, "Please same password", Toast.LENGTH_LONG).show();
+                    editTextRegisterConfirmPwd.requestFocus();
+
+                    editTextRegisterPwd.clearComposingText();
+                    editTextRegisterConfirmPwd.clearComposingText();
+                } else {
+                    textGender = radioButtonRegisterGenderSelected.getText().toString();
+                    progressBar.setVisibility(View.VISIBLE);
+                    registerReceiver(textFullName, textEmail, textDoB, textGender, textMobile, textPwd);
+
+
+                }
             }
 
         });
     }
+
+
+
+
 }
