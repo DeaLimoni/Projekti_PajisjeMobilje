80deb356c6e60ed1e1df6f1826195cba259b902a
Dea Limoni <dea.limoni@student.uni-pr.edu>
2024-12-19 18:12:50 +0100

    update

--- app/src/main/java/com/example/projekti_pajisjemobilje/UserProfileActivity.java
+++ app/src/main/java/com/example/projekti_pajisjemobilje/UserProfileActivity.java
@@ -1,6 +1,7 @@
 package com.example.projekti_pajisjemobilje;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +10,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class UserProfileActivity extends AppCompatActivity {
+private TextView textViewWelcome, textViewFullName, textViewEmail, textViewDoB, textViewGender, textViewMobile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+getSupportActionBar().setTitle("Home");
+
+
+
+
 
         }
     }
