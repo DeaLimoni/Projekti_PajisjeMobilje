1c2d085636940826154cd65b8d4433044b87a215
Dea Limoni <dea.limoni@student.uni-pr.edu>
2024-12-24 14:18:29 +0100

    update

--- app/src/main/java/com/example/projekti_pajisjemobilje/ChangePasswordActivity.java
+++ app/src/main/java/com/example/projekti_pajisjemobilje/ChangePasswordActivity.java
@@ -2,8 +2,10 @@ package com.example.projekti_pajisjemobilje;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -14,14 +16,23 @@ import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class ChangePasswordActivity extends AppCompatActivity {
- private FirebaseUser authProfile;
+ private FirebaseAuth authProfile;
+
+    private FirebaseUser firebaseUser;
  private EditText editTextpwdCurr, editTextPwdNew,editTextPwdConfirmNew;
    private TextView textViewAuthenticated;
    private Button buttonChangePwd, buttonReAuthenticate;
@@ -51,9 +62,70 @@ public class ChangePasswordActivity extends AppCompatActivity {
             editTextPwdNew.setEnabled(false);
             editTextPwdConfirmNew.setEnabled(false);
             buttonChangePwd.setEnabled(false);
+
+        authProfile = FirebaseAuth.getInstance();
+        firebaseUser = authProfile.getCurrentUser();
+
+
+        if(firebaseUser.equals("")){
+            Toast.makeText(ChangePasswordActivity.this, "Something went wrong!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent((ChangePasswordActivity.this, UserProfileActivity.class));
+            startActivity(intent);
+            finish();
+        }else {
+            reAuthenticate(firebaseUser);
             
+        }
 
     }
+
+    private void reAuthenticate(FirebaseUser firebaseUser) {
+   buttonReAuthenticate.setOnClickListener(new View.OnClickListener() {
+       @Override
+       public void onClick(View v) {
+           userPwdCurr = editTextpwdCurr.getText().toString();
+
+           if(TextUtils.isEmpty((userPwdCurr)){
+               Toast.makeText(ChangePasswordActivity.this, "Password is needed", Toast.LENGTH_SHORT).show();
+         editTextpwdCurr.setError("Please enter your current password to authenticate");
+         editTextpwdCurr.requestFocus();
+           } else {
+               progressBar.setVisibility(View.VISIBLE);
+               AuthCredential credential = EmailAuthProvider.getCredential(firebaseUser.getEmail(),userPwdCurr);
+
+               firebaseUser.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+                   @Override
+                   public void onComplete(@NonNull Task<Void> task) {
+                       if(task.isSuccessful()){
+                           progressBar.setVisibility(View.GONE);
+
+                            editTextpwdCurr.setEnabled(false);
+                            editTextPwdNew.setEnabled(true);
+                            editTextPwdConfirmNew.setEnabled(true);
+
+                            buttonReAuthenticate.setEnabled(false);
+                            buttonChangePwd.setEnabled(true);
+
+                            textViewAuthenticated.setText("You are authenticated/verified."+"You can change password now!");
+                           Toast.makeText(ChangePasswordActivity.this, "Password has been verified."+"Change password now", Toast.LENGTH_SHORT).show();
+                      buttonChangePwd.setBackgroundTintList(ContextCompat.getColorStateList(
+                              ChangePasswordActivity.this, R.color.dark_green));
+
+                      buttonChangePwd.setOnClickListener(new View.OnClickListener() {
+                          @Override
+                          public void onClick(View v) {
+                              chan
+                          }
+                      });
+                       }
+                   }
+               });
+
+           }
+       }
+   });
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.common_menu, menu);
