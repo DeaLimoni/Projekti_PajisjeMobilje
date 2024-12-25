31cea2e6fd767a1ca4735a3373708b6a1ad901b1
Dea Limoni <dea.limoni@student.uni-pr.edu>
2024-12-25 15:25:06 +0100

    update

--- app/src/main/java/com/example/projekti_pajisjemobilje/MainActivity.java
+++ app/src/main/java/com/example/projekti_pajisjemobilje/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -38,8 +39,8 @@ public class MainActivity extends AppCompatActivity {
 
 
         // Hap aktivitetin e regjistrimit
-        Button buttonRegister = findViewById(R.id.button_register);
-        buttonRegister.setOnClickListener(new View.OnClickListener() {
+       TextView textViewRegister = findViewById(R.id.textView_register_link);
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
